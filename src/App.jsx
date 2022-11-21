94c6705e12d2121934e08e3b738ebe67f451a6a3
Daniel Markow <daniel.markow@outlook.com>
2022-11-21 21:06:22 +0100

    passing mutation state through components or context does not seem to be possible right now - maybe only get userdata via /api/users/me?

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,21 @@ import "./App.css";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Navbar from "./components/common/Navbar";
-
-// const saveToken = (token) => {
-//   localStorage.setItem("goals-token", token);
-// };
+import UserProvider from "./context/UserProvider";
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div>
-        <Navbar />
-        <Route path="/" component={Home} />
-        <Route path="/login" component={Login} />
-      </div>
-    </QueryClientProvider>
+    <UserProvider>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <Navbar />
+          <Route path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </div>
+      </QueryClientProvider>
+    </UserProvider>
   );
 }
 
---
+++ src/context/UserProvider.jsx
@@ -0,0 +1,49 @@
+import { useMutation } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+import { UserContext } from "./UserContext";
+
+function UserProvider({ children }) {
+  const [userdata, setUserdata] = useState({});
+
+  useEffect(() => {
+    if (loginMutation.isSuccess) {
+      const { id, name, email, token } = loginMutation.data.data;
+      localStorage.setItem("goals-token", token);
+      setUserdata({
+        id,
+        name,
+        email,
+      });
+    }
+  }, [loginMutation.data]);
+
+  const loginMutation = useMutation({
+    mutationFn: (data) => {
+      return axios.post("http://localhost:5001/api/users/login", {
+        email: data.emailInp,
+        password: data.pwInp,
+      });
+    },
+  });
+
+  const handleLogin = (data) => {
+    loginMutation.mutate(data);
+    return loginMutation;
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("goals-token");
+    setUserdata({});
+  };
+
+  const value = {
+    userdata,
+    onLogin: handleLogin,
+    onLogout: handleLogout,
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
+
+export default UserProvider;
