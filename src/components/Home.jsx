424de3b4696151a570d68dc1f542b53b881b530e
Daniel Markow <daniel.markow@outlook.com>
2022-11-21 19:46:44 +0100

    delete goal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
-import { useLocation } from "wouter";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+// import { useLocation } from "wouter";
 
 function Home() {
-  const [location, setLocation] = useLocation();
+  // const [location, setLocation] = useLocation();
+  const queryClient = useQueryClient();
   const enableQuery = localStorage.getItem("goals-token") ? true : false;
 
   const goals = useQuery({
@@ -19,28 +20,46 @@ function Home() {
     enabled: enableQuery,
   });
 
+  const removeGoal = useMutation({
+    mutationFn: (goalId) => {
+      const token = localStorage.getItem("goals-token");
+      axios.delete(`http://localhost:5001/api/goals/${goalId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+    },
+    onSuccess: queryClient.invalidateQueries(["goals"]),
+  });
+
+  const onClickDel = (event) => {
+    event.preventDefault;
+    removeGoal.mutate(event.target.id);
+  };
+
   return (
     <>
       {goals.isLoading && enableQuery && <p>loading...</p>}
       {goals.isError && enableQuery && <p>an error occured...</p>}
       {goals.isSuccess && enableQuery && (
-        <ul>
-          {goals.data.data.map((goal, i) => (
-            <li key={i}>{goal.goaltext}</li>
-          ))}
-        </ul>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <ul>
+              {goals.data.data.map((goal, i) => (
+                <li key={i}>
+                  {i + 1} - {goal.goaltext} &nbsp;
+                  <button
+                    id={goal.id}
+                    className="btn btn-xs"
+                    onClick={onClickDel}
+                  >
+                    X
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       )}
       {!enableQuery && <p>please log in</p>}
-      {/* {!localStorage.getItem("goals-token") && (
-        <button
-          className="btn btn-outline"
-          onClick={() => {
-            setLocation("/login");
-          }}
-        >
-          Login
-        </button>
-      )} */}
     </>
   );
 }
